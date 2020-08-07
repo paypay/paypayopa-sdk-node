dbc4210e78fc8b827bd0b02dd8070f9395f29243
Gopalakrishnan.R <t-gopalakrishnan.r@paypay-corp.co.jp>
2020-08-07 17:49:29 +0530

    TAG20200807-01 : Test case implemation for hmac auth header using jest

--- __tests__/paypay-rest-sdk.test.ts
+++ __tests__/paypay-rest-sdk.test.ts
@@ -1,11 +1,17 @@
 import { payPayRestSDK } from '../src/lib/paypay-rest-sdk';
 
-payPayRestSDK.configure({
-  clientId: "a_1obUPwUWdx_2uR6",
-  clientSecret: "L24NeSrLDnpxEKq6z4y1QTuhl1/j4RdYZsLeN6cZ",
-});
-
 describe('Generating PAYPAY TEST CASE', (): any => {
+    // Dummy Client Key Generator for testing purpose
+    const keyGenerate = (keyLength: number) => {
+        let result           = '';
+        let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz_0123456789';
+        let charactersLength = characters.length;
+        for (let i = 0; i < keyLength; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+
     test('HMAC createAuthHeader Methods', (): any => {
         const body = {
             "merchantPaymentId":"Test123",
@@ -14,11 +20,12 @@ describe('Generating PAYPAY TEST CASE', (): any => {
             "orderItems":[{"name":"Cake","quantity":1,"unitPrice":{"amount":20,"currency":"JPY"}}]
         };  
         const auth = {
-            clientId: "a_1obUPwUWdx_2uR6",
-            clientSecret: "L24NeSrLDnpxEKq6z4y1QTuhl1/j4RdYZsLeN6cZ"
+            clientId: keyGenerate(17),
+            clientSecret: keyGenerate(40)
         };
         const exampleProto = Object.getPrototypeOf(payPayRestSDK);
         const token = exampleProto.constructor.createAuthHeader("POST", "/v2/codes", body, auth);
+        console.log(token);
         const tokenSplit = token ? token.split(":") : [];
         expect(tokenSplit).toHaveLength(6);
     });
